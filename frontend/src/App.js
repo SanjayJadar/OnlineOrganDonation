5776891cef99cf5295c53de269894c9f30d6d6be
Sanjay <sanjayjadar53@gmail.com>
2023-12-14 22:56:53 +0530

    Designed Main Post Page and MainDetail page to show all the details of a single post

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './Components/main';
+import MainDetailPage from './Components/mainDetail';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MainPage/> */}
+      <MainDetailPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/main.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div className="main-page-container">
+      {/* Card 1 */}
+      <div className="card-container">
+        <div className="card">
+          <img className="card-image" src="https://i.pinimg.com/236x/25/5e/7b/255e7b5049fcd0db9ccbaf9a4368beda.jpg" alt="Image 1" />
+          <div className="card-content">
+            <h2 className="card-title">Card 1 Title</h2>
+            <p className="card-description">
+              Description for Card 1. Add more details here as needed. This is a longer description to demonstrate truncation after a certain number of lines. It should be of the same height and width.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Card 2 */}
+      <div className="card-container">
+        <div className="card">
+          <img className="card-image" src="https://i.pinimg.com/236x/e6/ef/d4/e6efd4ea7c5a9a291b20c363521fbcf5.jpg" alt="Image 2" />
+          <div className="card-content">
+            <h2 className="card-title">Card 2 Title</h2>
+            <p className="card-description">
+              Description for Card 2. Add more details here as needed. This is a longer description to demonstrate truncation after a certain number of lines. It should be of the same height and width.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Card 2 */}
+      <div className="card-container">
+        <div className="card">
+          <img className="card-image" src="https://i.pinimg.com/236x/a0/08/f6/a008f65c4fe17d5deff055e70c4b54ed.jpg" alt="Image 2" />
+          <div className="card-content">
+            <h2 className="card-title">Card 2 Title</h2>
+            <p className="card-description">
+              Description for Card 2. Add more details here as needed. This is a longer description to demonstrate truncation after a certain number of lines. It should be of the same height and width.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Card 2 */}
+      <div className="card-container">
+        <div className="card">
+          <img className="card-image" src="https://i.pinimg.com/236x/25/ba/22/25ba220b089ae2597186dc12c30bd5b1.jpg" alt="Image 2" />
+          <div className="card-content">
+            <h2 className="card-title">Card 2 Title</h2>
+            <p className="card-description">
+              Description for Card 2. Add more details here as needed. This is a longer description to demonstrate truncation after a certain number of lines. It should be of the same height and width.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Card 3 */}
+      <div className="card-container">
+        <div className="card">
+          <img className="card-image" src="https://i.pinimg.com/236x/ec/40/7e/ec407e444fad754e8ef38a07f521082d.jpg" alt="Image 3" />
+          <div className="card-content">
+            <h2 className="card-title">Card 3 Title</h2>
+            <p className="card-description">
+              Description for Card 3. Add more details here as needed. This is a longer description to demonstrate truncation after a certain number of lines. It should be of the same height and width.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
